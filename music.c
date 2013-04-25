6d2312343e06cdc13f5fe8e4c5293649370db061
Ellen Marie Dash <me@duckie.co>
2013-04-24 22:23:06 -0400

    HOLY CRAP THIS IS FREAKING AMAZING I DON'T EVEN HOW DID I DO THIS

--- music.c
+++ music.c
@@ -3,11 +3,79 @@
   exit
 #endif
 
-int main(i) {
-  for(i=0; i < 32000; i++) {
+/* I USE THE BEST FUNCTION NAMES. */
+
+int i = 0;
+
+void dadadidi() {
+  int end = i + 8000;
+
+  for(; i < end; i++) {
     putchar(i * (i >> 17 | i >> 9) & 46 & i >> 3);
-    putchar(i * (i >> 5 | i >> 3));
+    putchar(i * (i >> 5  | i >> 3));
   }
+}
+
+void dadadidi2() {
+  int end = i + 8000;
+
+  for(; i < end; i++) {
+    putchar(i * (i >> 17 | i >> 9) & 46 & i >> 3);
+    putchar(i * (i >> 6 | i >> 4));
+  }
+}
+
+void woosh() {
+  int end = i + 8000;
+
+  for(; i < end; i++) {
+    putchar(i * (i >> 12 | i >> 3) & 30 & i << 4);
+    putchar(i * (i >> 30 | i >> 5));
+  }
+}
+
+void something() {
+  int end = i + 8000;
+  for (; i < end; i++) {
+    putchar(i * (i >> 10 | i >> 3));
+    putchar(i * (i >> 6 | i >> 4));
+  }
+}
+
+void something2() {
+  int end = i + 8000;
+
+  for (; i < end; i++) {
+    putchar(i * (i >> 5 | i >> 3) & 32 & i >> 2);
+    putchar(i * (i >> 6 | i >> 4));
+  }
+}
+
+void something2woosh() {
+  int end = i + 18000;
+
+  for(; i < end; i++) {
+    putchar(i * (i >> 5 | i >> 3) & 32 & i >> 2);
+    //putchar(i * (i >> 12 | i >> 3) & 30 & i << 4);
+    //putchar(i * (i >> 30 | i >> 5));
+    putchar(i * (i >> 30 | i >> 5) & i >> 4);
+  }
+}
+
+int main() {
+  int j;
+
+  for (j = 0; j < 2; j++)
+    dadadidi();
+
+  woosh();
+  something();
+  dadadidi2();
+  something();
+  dadadidi();
+  something2();
+  something();
+  something2woosh();
 
   return 0;
 }
